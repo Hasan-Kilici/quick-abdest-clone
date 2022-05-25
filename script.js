23f8a62085c052dde8a34ecf97e313cba82fa7a1
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-05-25 06:05:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function abdest() {
+  document.getElementById("abdestli").src = "https://cdn.glitch.global/410d25a7-9a8e-4304-bf4a-f4ced2d16cf9/aa734df3-f172-45fc-8e9b-619691f5e4de.image.png?v=1653423302525";
+  document.getElementById("abdest-text").innerHTML = "A B D E S T L E N D I N";
+  document.getElementById("audio").play()
+  setTimeout(()=>{
+  document.getElementById("abdestli").src = "https://cdn.glitch.global/410d25a7-9a8e-4304-bf4a-f4ced2d16cf9/146a48e3-ad99-4f18-afcf-e84bb125ab4e.image.png?v=1653423296188";
+  document.getElementById("abdest-text").innerHTML = "";  
+  },22000);
+}
